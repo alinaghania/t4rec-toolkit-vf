8c25e6a51caa18df021009dfb776b0de152135b4
alinaghania <115467647+alinaghania@users.noreply.github.com>
2025-08-25 16:33:28 +0200

    Update __init__.py

--- t4rec_toolkit/models/__init__.py
+++ t4rec_toolkit/models/__init__.py
@@ -2,16 +2,18 @@ from .registry import ModelRegistry, get_available_models, create_model
 from .base_builder import BaseModelBuilder
 from .xlnet_builder import XLNetModelBuilder
 from .gpt2_builder import GPT2ModelBuilder
+from .t4rec_advanced import T4RecAdvancedModel
 
 # Registry global pré-configuré
 registry = ModelRegistry()
 
 __all__ = [
-    'ModelRegistry',
-    'BaseModelBuilder',
-    'XLNetModelBuilder', 
-    'GPT2ModelBuilder',
-    'get_available_models',
-    'create_model',
-    'registry'
+    "ModelRegistry",
+    "BaseModelBuilder",
+    "XLNetModelBuilder",
+    "GPT2ModelBuilder",
+    "T4RecAdvancedModel",
+    "get_available_models",
+    "create_model",
+    "registry",
 ]
